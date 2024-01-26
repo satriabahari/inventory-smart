80640a9ecf58882ee4423cd2316534e08f318e9f
Nurman06 <nurmanmuhammad52@gmail.com>
2024-01-27 00:00:22 +0700

    pesan_commit

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,7 +4,6 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  @vite('resources/css/app.css')
   <title>Project Study Club</title>
 </head>
 <body>
