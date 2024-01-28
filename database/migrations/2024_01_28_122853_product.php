e8051727b455c6e2e1c0d13731cdb9fa9ecd4eb8
Satria Bahari <satriaabaharii@gmail.com>
2024-01-28 22:20:58 +0700

    Refactor: Added some migration

--- database/migrations/2024_01_28_122853_product.php
+++ database/migrations/2024_01_28_122853_product.php
@@ -11,10 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create("user", function (Blueprint $table) {
+        Schema::create("product", function (Blueprint $table) {
             $table->id();
-            $table->string("email")->unique;
-            $table->string("password");
+            $table->string("name");
+            $table->string("description");
+            $table->integer("stock");
             $table->timestamps();
         });
     }
@@ -24,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema:: dropIfExists("user");
+        Schema:: dropIfExists("product");
     }
 };
