b9b4e26f948b45724e4c6d21b4937989afdf4321
Satria Bahari <satriaabaharii@gmail.com>
2024-01-30 11:05:43 +0700

    Style: Changed some code

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    // darkMode: "class",
+    darkMode: "class",
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
