de4ac54c630408aac6e77d3fd0914dfd07ec2e25
Nurman06 <nurmanmuhammad52@gmail.com>
2024-02-01 19:33:40 +0700

    Refactor: Added some code

--- resources/views/product_keluar/create.blade.php
+++ resources/views/product_keluar/create.blade.php
@@ -50,5 +50,13 @@
                 </form>
             </div>
         </div>
+        @if(session('error'))
+            <script>
+                // JavaScript to show alert if stock is not sufficient
+                document.addEventListener('DOMContentLoaded', function () {
+                    alert("{{ session('error') }}");
+                });
+            </script>
+        @endif
     </section>
 </x-app-layout>
