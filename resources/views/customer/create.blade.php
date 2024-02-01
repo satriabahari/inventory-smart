67bcc5508ada7bd7660e8c845d5fe8337ccde92f
Nurman06 <nurmanmuhammad52@gmail.com>
2024-02-01 12:46:33 +0700

    Refactor: Changed some code

--- resources/views/customer/create.blade.php
+++ resources/views/customer/create.blade.php
@@ -1,86 +1,41 @@
-{{-- <x-app-layout>
-  <section class="p-8">
-    <div class="container mx-auto">
-        <div class="bg-white dark:bg-gray-800 p-8 rounded-lg shadow-md">
-            <h1 class="text-2xl font-bold mb-6 dark:text-gray-50">Tambah Data</h1>
-    
-            <!-- Form -->
-            <form action="{{ route('customer.store') }}" method="POST">
-                @csrf
-    
-                <!-- Nama -->
-                <div class="mb-4">
-                    <label for="name" class="block text-gray-600 font-medium mb-2">Name</label>
-                    <input type="text" name="name" id="name" class="w-full border-gray-300 rounded-md p-2" required>
-                </div>
-    
-                <!-- Deskripsi -->
-                <div class="mb-4">
-                    <label for="email" class="block text-gray-600 font-medium mb-2">Email</label>
-                    <input type="email" name="email" id="email" class="w-full border-gray-300 rounded-md p-2" required>
-                </div>
-    
-                <!-- Stock -->
-                <div class="mb-6">
-                    <label for="address" class="block text-gray-600 font-medium mb-2">Address</label>
-                    <textarea name="address" id="address" class="w-full border-gray-300 rounded-md p-2" rows="4" required></textarea>
-                </div>
-
-                <div class="mb-6">
-                    <label for="phone" class="block text-gray-600 font-medium mb-2">Phone</label>
-                    <input type="number" name="phone" id="phone" class="w-full border-gray-300 rounded-md p-2" required>
-                </div>
-    
-                <!-- Tombol Submit -->
-                <div>
-                    <button type="submit" class="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue active:bg-blue-800">
-                        Simpan
-                    </button>
-                </div>
-            </form>
-        </div>
-      </div>
-  </section>
-</x-app-layout> --}}
-
 <x-app-layout>
     <section class="p-8">
         <div class="container mx-auto">
             <div class="bg-white dark:bg-gray-800 p-8 rounded-lg shadow-md">
-                <h1 class="text-2xl font-bold mb-8 text-gray-500 dark:text-gray-300">Tambah Data Customer</h1>
-        
+                <h1 class="text-2xl font-bold mb-8 text-gray-500 dark:text-gray-300">Create Customer</h1>
+
                 <!-- Form -->
-                <form action="{{ route('product.store') }}" method="POST" class="space-y-4">
+                <form action="{{ route('customer.store') }}" method="POST" class="space-y-4">
                     @csrf
-        
-                    <!-- Nama -->
+
+                    <!-- Name -->
                     <div class="grid grid-cols-[1fr_2fr] items-center">
                         <label for="name" class="block text-gray-500 dark:text-gray-300 font-medium mb-2">Name<x-star-required/></label>
-                        <input type="text" name="name" id="name" class="w-full border-gray-300 dark:bg-gray-700 dark:text-gray-50 bg-gray-100 rounded-md p-2" placeholder="Text input" required>
+                        <input type="text" name="name" id="name" class="w-full border-gray-300 dark:bg-gray-700 dark:text-gray-50 bg-gray-100 rounded-md p-2" placeholder="Name" required>
                     </div>
 
-                    <!-- Nama -->
+                    <!-- Address -->
+                    <div class="grid grid-cols-[1fr_2fr] items-center">
+                        <label for="address" class="block text-gray-500 dark:text-gray-300 font-medium mb-2">Address<x-star-required/></label>
+                        <textarea name="address" id="address" class="w-full border-gray-300 dark:bg-gray-700 dark:text-gray-50 bg-gray-100 rounded-md p-2" rows="3" placeholder="Address" required></textarea>
+                    </div>
+
+                    <!-- Email -->
                     <div class="grid grid-cols-[1fr_2fr] items-center">
                         <label for="email" class="block text-gray-500 dark:text-gray-300 font-medium mb-2">Email<x-star-required/></label>
-                        <input type="text" name="email" id="email" class="w-full border-gray-300 dark:bg-gray-700 dark:text-gray-50 bg-gray-100 rounded-md p-2" placeholder="Text input" required>
-                    </div>
-        
-                    <!-- Deskripsi -->
-                    <div class=" grid grid-cols-[1fr_2fr] items-kcenter">
-                        <label for="address" class="block text-gray-500 dark:text-gray-300 font-medium mb-2">Address<x-star-required/></label>
-                        <textarea name="address" id="address" class="w-full border-gray-300 dark:bg-gray-700 dark:text-gray-50 bg-gray-100 rounded-md p-2" rows="3" placeholder="address" required></textarea>
+                        <input type="email" name="email" id="email" class="w-full border-gray-300 dark:bg-gray-700 dark:text-gray-50 bg-gray-100 rounded-md p-2" placeholder="Email" required>
                     </div>
 
-                    <!-- price -->
-                    <div class="grid grid-cols-[1fr_2fr] items-center ">
+                    <!-- Phone -->
+                    <div class="grid grid-cols-[1fr_2fr] items-center">
                         <label for="phone" class="block text-gray-500 dark:text-gray-300 font-medium mb-2">Phone<x-star-required/></label>
-                        <input type="number" name="phone" id="phone" class="w-full border-gray-300 dark:bg-gray-700 dark:text-gray-50 bg-gray-100 rounded-md p-2" placeholder="phone" required>
+                        <input type="tel" name="phone" id="phone" class="w-full border-gray-300 dark:bg-gray-700 dark:text-gray-50 bg-gray-100 rounded-md p-2" placeholder="Phone" required>
                     </div>
-        
+
                     <!-- Tombol Submit -->
                     <div>
-                        <button type="submit" class="flex items-center space-x-2 bg-blue-500 text-white py-2 px-4 rounded-md ">
-                            <p>Submit</p>
+                        <button type="submit" class="flex items-center space-x-2 bg-blue-500 text-gray-50 dark:text-gray-200 py-2 px-4 rounded-md">
+                            <p>Create</p>
                             <i class="fa-regular fa-circle-check"></i>
                         </button>
                     </div>
--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -1,105 +1,46 @@
-{{-- <x-app-layout>
-  <section class="h-full p-4 space-y-4">
-    <div class="flex justify-between">
-      <x-button route="customer.create" color="blue" text="Create" icon="fa-plus"/>
-    </div>
-    @if (count($datas) > 0)
-      <div class="flex flex-col justify-between h-full bg-neutral-50 dark:bg-gray-800 rounded-xl p-2">
-        <table class="w-full table-auto">
-          <thead>
-            <tr>
-              <th class="text-neutral-900 bg-neutral-50 dark:bg-gray-800 dark:text-neutral-50 py-1 px-4 text-start text-sm">No</th>
-              <th class="text-neutral-900 bg-neutral-50 dark:bg-gray-800 dark:text-neutral-50 py-1 px-4 text-start text-sm">Name</th>
-              <th class="text-neutral-900 bg-neutral-50 dark:bg-gray-800 dark:text-neutral-50 py-1 px-4 text-start text-sm">Address</th>
-              <th class="text-neutral-900 bg-neutral-50 dark:bg-gray-800 dark:text-neutral-50 py-1 px-4 text-start text-sm">Email</th>
-              <th class="text-neutral-900 bg-neutral-50 dark:bg-gray-800 dark:text-neutral-50 py-1 px-4 text-start text-sm">Phone</th>
-              <th class="text-neutral-900 bg-neutral-50 dark:bg-gray-800 dark:text-neutral-50 py-1 px-4 text-start text-sm">Actions</th>
-            </tr> 
-          </thead>
-          <tbody>
-            @foreach ($datas as $data)
-            <tr>
-              <td class="text-neutral-500 bg-neutral-50 dark:bg-gray-800 dark:text-neutral-300 py-1 px-4 text-sm">{{ $data->id }}</td>
-              <td class="text-neutral-500 bg-neutral-50 dark:bg-gray-800 dark:text-neutral-300 py-1 px-4 text-sm">{{ $data->name }}</td>
-              <td class="text-neutral-500 bg-neutral-50 dark:bg-gray-800 dark:text-neutral-300 py-1 px-4 text-sm">{{ $data->address }}</td>
-              <td class="text-neutral-500 bg-neutral-50 dark:bg-gray-800 dark:text-neutral-300 py-1 px-4 text-sm">{{ $data->email }}</td>
-              <td class="text-neutral-500 bg-neutral-50 dark:bg-gray-800 dark:text-neutral-300 py-1 px-4 text-sm">{{ $data->phone }}</td>
-              <td class="text-neutral-50 bg-neutral-50 dark:bg-gray-800 dark:text-neutral-50 py-1 px-4 flex space-x-4">
-                <x-button :route="route('customer.edit', $data->id)" color="green" text="Edit" icon="fa-pen-to-square"/>
-                <form action="{{route("customer.destroy", $data->id)}}" method="POST">
-                  @csrf
-                  @method("DELETE")
-                  <x-button :route="route('customer.destroy', $data->id)" color="red" text="Delete" icon="fa-trash"/>
-                </form>
-              </td>
-            </tr>
-            @endforeach
-          </tbody>
-        </table>
-
-        <div class="py-2 px-4">
-          {{ $datas->links() }}
-        </div>
-
-      @else
-        <x-data-not-found/>
-      @endif
-      
-    </div>
-  </section>
-</x-app-layout> --}}
-
 <x-app-layout>
-  <section class="h-full p-4 space-y-4">
-    <div class="flex justify-between">
-      <x-button :route="route('customer.create')" color="blue" text="Create" icon="fa-plus"/>
-    </div>
-    @if (count($datas) > 0)
-      <div class="flex flex-col justify-between h-full bg-neutral-50 dark:bg-gray-800 rounded-xl p-2">
-        <table class="w-full table-auto">
-          <thead>
-            <tr>
-              <th class="text-neutral-900 bg-neutral-50 dark:bg-gray-800 dark:text-neutral-50 py-1 px-4 text-start text-sm">No</th>
-              <th class="text-neutral-900 bg-neutral-50 dark:bg-gray-800 dark:text-neutral-50 py-1 px-4 text-start text-sm">Name</th>
-              <th class="text-neutral-900 bg-neutral-50 dark:bg-gray-800 dark:text-neutral-50 py-1 px-4 text-start text-sm">Address</th>
-              <th class="text-neutral-900 bg-neutral-50 dark:bg-gray-800 dark:text-neutral-50 py-1 px-4 text-start text-sm">Email</th>
-              <th class="text-neutral-900 bg-neutral-50 dark:bg-gray-800 dark:text-neutral-50 py-1 px-4 text-start text-sm">Phone</th>
-              <th class="text-neutral-900 bg-neutral-50 dark:bg-gray-800 dark:text-neutral-50 py-1 px-4 text-start text-sm">Actions</th>
-            </tr> 
-          </thead>
-          <tbody>
-            @foreach ($datas as $data)
-            <tr>
-              <td class="text-neutral-500 bg-neutral-50 dark:bg-gray-800 dark:text-neutral-300 py-1 px-4 text-sm">{{ $data->id }}</td>
-              <td class="text-neutral-500 bg-neutral-50 dark:bg-gray-800 dark:text-neutral-300 py-1 px-4 text-sm">{{ $data->name }}</td>
-              <td class="text-neutral-500 bg-neutral-50 dark:bg-gray-800 dark:text-neutral-300 py-1 px-4 text-sm">{{ $data->address }}</td>
-              <td class="text-neutral-500 bg-neutral-50 dark:bg-gray-800 dark:text-neutral-300 py-1 px-4 text-sm">{{ $data->email }}</td>
-              <td class="text-neutral-500 bg-neutral-50 dark:bg-gray-800 dark:text-neutral-300 py-1 px-4 text-sm">{{ $data->phone }}</td>
-              <td class="text-neutral-50 bg-neutral-50 dark:bg-gray-800 dark:text-neutral-50 py-1 px-4 flex space-x-4">
-                <x-button :route="route('customer.edit', $data->id)" color="green" text="Edit" icon="fa-pen-to-square"/>
-
-                <form action="{{route('customer.destroy', $data->id)}}" method="POST">
-                  @csrf
-                  @method("DELETE")
-                  <button type="submit">
-                    <x-button color="red" text="Delete" icon="fa-trash"/>
-                  </button>
-                </form>
-              </td>
-            </tr>
-            @endforeach
-          </tbody>
-        </table>
-
-        <div class="py-2 px-4">
-          {{ $datas->links() }}
+    <section class="h-full p-4 space-y-4">
+        <div class="flex justify-between">
+            <x-button :route="route('customer.create')" color="blue" text="Create" icon="fa-plus"/>
         </div>
-
-      @else
-        <x-data-not-found/>
-      @endif
-      
-    </div>
-  </section>
+        @if (count($data) > 0)
+            <div class="flex flex-col justify-between h-full bg-neutral-50 dark:bg-gray-800 rounded-xl p-2">
+                <table class="w-full table-auto">
+                    <thead>
+                        <tr>
+                            <th class="text-neutral-900 bg-neutral-50 dark:bg-gray-800 dark:text-neutral-50 py-1 px-4 text-start text-sm">No</th>
+                            <th class="text-neutral-900 bg-neutral-50 dark:bg-gray-800 dark:text-neutral-50 py-1 px-4 text-start text-sm">Name</th>
+                            <th class="text-neutral-900 bg-neutral-50 dark:bg-gray-800 dark:text-neutral-50 py-1 px-4 text-start text-sm">Address</th>
+                            <th class="text-neutral-900 bg-neutral-50 dark:bg-gray-800 dark:text-neutral-50 py-1 px-4 text-start text-sm">Email</th>
+                            <th class="text-neutral-900 bg-neutral-50 dark:bg-gray-800 dark:text-neutral-50 py-1 px-4 text-start text-sm">Phone</th>
+                            <th class="text-neutral-900 bg-neutral-50 dark:bg-gray-800 dark:text-neutral-50 py-1 px-4 text-start text-sm">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($data as $item)
+                            <tr>
+                                <td class="text-neutral-500 bg-neutral-50 dark:bg-gray-800 dark:text-neutral-300 py-1 px-4 text-sm">{{ $loop->iteration }}</td>
+                                <td class="text-neutral-500 bg-neutral-50 dark:bg-gray-800 dark:text-neutral-300 py-1 px-4 text-sm">{{ $item->name }}</td>
+                                <td class="text-neutral-500 bg-neutral-50 dark:bg-gray-800 dark:text-neutral-300 py-1 px-4 text-sm">{{ $item->address }}</td>
+                                <td class="text-neutral-500 bg-neutral-50 dark:bg-gray-800 dark:text-neutral-300 py-1 px-4 text-sm">{{ $item->email }}</td>
+                                <td class="text-neutral-500 bg-neutral-50 dark:bg-gray-800 dark:text-neutral-300 py-1 px-4 text-sm">{{ $item->phone }}</td>
+                                <td class="text-neutral-50 bg-neutral-50 dark:bg-gray-800 dark:text-neutral-50 py-1 px-4 flex space-x-4">
+                                    <x-button :route="route('customer.edit', $item->id)" color="green" text="Edit" icon="fa-pen-to-square"/>
+                                    <form action="{{ route('customer.destroy', $item->id) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit">
+                                            <x-button color="red" text="Delete" icon="fa-trash"/>
+                                        </button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        @else
+            <x-data-not-found/>
+        @endif
+    </section>
 </x-app-layout>
-
