2cb2651ccefd1fcebf27cf88974f25e5cc54f36e
Nurman06 <nurmanmuhammad52@gmail.com>
2024-02-01 19:30:41 +0700

    Refactor: Changed some code

--- app/Http/Controllers/ProductKeluarController.php
+++ app/Http/Controllers/ProductKeluarController.php
@@ -30,32 +30,23 @@ class ProductKeluarController extends Controller
             'stock' => 'required|numeric',
             'tanggal' => 'required|date',
         ]);
-
+    
         // Validasi stok mencukupi
         $product = Product::find($request->product_id);
-
+    
         if (!$product || $request->stock > $product->stock) {
             return redirect()->route('product_keluar.create')->with('error', 'Stock tidak mencukupi.');
         }
-
+    
         // Jika stok mencukupi, simpan data produk keluar
         $entry = Product_Keluar::create($request->all());
-
+    
         // Kurangi stok produk
         $product->update([
             'stock' => $product->stock - $request->stock,
         ]);
-
+    
         return redirect()->route('product_keluar.index')->with('success', 'Product Keluar created successfully');
-    }
-
-    public function edit($id)
-    {
-        $entry = Product_Keluar::findOrFail($id);
-        $products = Product::all();
-        $customers = Customer::all();
-
-        return view('product_keluar.edit', compact('entry', 'products', 'customers'));
     }
 
     public function update(Request $request, $id)
