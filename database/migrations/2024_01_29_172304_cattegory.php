a87b12c9cd74f111b8bd46ed39280612ed1cf5d8
Nurman06 <nurmanmuhammad52@gmail.com>
2024-02-01 12:48:51 +0700

    Refactor: Changed some code

--- database/migrations/2024_01_29_172304_cattegory.php
+++ database/migrations/2024_01_29_172304_cattegory.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create("cattegories", function (Blueprint $table) {
-            $table->id();
-            $table->string("name");
+        Schema::create('cattegories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('cattegories');
     }
 };
